29b4f6643afb126a23f7df3b7780485dc985cd37
Egor Ivanov <rust_lang_lover@proton.me>
2024-12-22 20:11:45 +0300

    // added password

--- myapi/alchemy_models/user_model.py
+++ myapi/alchemy_models/user_model.py
@@ -7,4 +7,5 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50))  # Specify a length for VARCHAR
+    password = Column(String(50))
     time = Column(DateTime, default=func.now())  # Correctly define the column
