055d63b58d2e3eb19efa65b9459517b8d77ad16f
Egor Ivanov <rust_lang_lover@proton.me>
2024-12-22 20:37:07 +0300

    // added token column

--- myapi/entities/user_response.py
+++ myapi/entities/user_response.py
@@ -1,10 +1,14 @@
 from datetime import datetime
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class UserResponse(BaseModel):
     id : int
     name : str
+    password : str
+    access_token : Optional[str] = None
     time : datetime
     class Config:
         orm_mode = True
