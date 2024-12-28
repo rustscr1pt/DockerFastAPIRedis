e7fe42748fa90feea515385f4d899051f070a893
Egor Ivanov <rust_lang_lover@proton.me>
2024-12-28 20:26:57 +0300

    // added celery, needs further set up

---
+++ myapi/celery_base.py
@@ -0,0 +1,7 @@
+from celery import Celery
+
+app = Celery(
+    'myapi',
+    broker='redis://redis_manager:6379/0',
+    backend='redis://redis_manager:6379/0'
+)
---
+++ myapi/tasks.py
@@ -0,0 +1,9 @@
+from celery_base import app
+
+@app.task
+def add(x, y):
+    return x + y
+
+@app.task
+def send_email():
+    print("Sending email")
