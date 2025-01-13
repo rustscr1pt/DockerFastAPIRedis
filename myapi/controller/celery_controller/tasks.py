2b7bc9c98a12c8b71864dcfe5a54911d600bc5d2
Egor Ivanov <rust_lang_lover@proton.me>
2025-01-13 20:26:57 +0300

    //

--- myapi/controller/celery_controller/tasks.py
+++ myapi/controller/celery_controller/tasks.py
@@ -27,3 +27,5 @@ def fetch_users_from_db():
         user_response = UserResponse.from_orm(user)
         redis_client.set(f"user:{user.id}", user_response.json(), ex=300)
     return [user_response.dict() for user in users]
+
+
